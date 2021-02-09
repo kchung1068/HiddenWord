1747557de6f74bfe56295c2d759114f3e21d24f3
kchung1068 <kchung1068@stu.d214.org>
2021-02-09 15:28:47 -0600

    c1

--- src/HiddenWord.java
+++ src/HiddenWord.java
@@ -9,10 +9,10 @@ class HiddenWord {
         for(int x = 0; x < guessedWord.length(); x += 1 ) {
             if (guessedWord.substring(x, x+1 ).equals(hiddenWord.substring(x,x +1) ) ) {
                 hint += guessedWord.substring(x ,x +1);
-            } else if (hiddenWord.contains(guessedWord.substring(x, x+1) ) && !(guessedWord.substring(x,x+1).equals(hiddenWord.substring(x,x+1) )   ) ) {
-                hint += "+";
-            } else {
+            } else if (hiddenWord.indexOf(guessedWord.substring(x,x+1)) == -1  ) {
                 hint += "*";
+            } else {
+                hint += "+";
             }
         }   return hint;
     }
